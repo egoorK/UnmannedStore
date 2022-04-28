d31df5366f9b800ec1987f3abece917bc24d188a
Egor <71252057+EgorKurta@users.noreply.github.com>
2022-04-28 21:34:42 +0500

    Добавлены модели данных

---
+++ Clients/Core/Clients.Application/Features/Accounts/Commands/DeleteAccount/DeleteAccountCommand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Clients.Application.Features.Accounts.Commands.DeleteAccount
+{
+    class DeleteAccountCommand
+    {
+        public Guid Account_ID { get; set; }
+    }
+}
---
+++ Clients/Core/Clients.Application/Features/Accounts/Commands/UpdateAccount/UpdateAccountCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clients.Application.Features.Accounts.Commands.UpdateAccount
+{
+    class UpdateAccountCommand
+    {
+        public Guid Account_ID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime Date_of_Birth { get; set; }
+        public string Phone_number { get; set; }
+    }
+}
