5682e9d98a69d55745d639bcfa255317af7666e8
Egor <71252057+EgorKurta@users.noreply.github.com>
2022-05-10 21:49:59 +0500

    Комментарий

--- Clients/Core/Clients.Application/Features/Accounts/Commands/CreateAccount/CreateAccountCommandHandler.cs
+++ Clients/Core/Clients.Application/Features/Accounts/Commands/CreateAccount/CreateAccountCommandHandler.cs
@@ -22,8 +22,9 @@ namespace Clients.Application.Features.Accounts.Commands.CreateAccount
         public async Task<Guid> Handle(CreateAccountCommand request, CancellationToken cancellationToken) // Реализует обработку запроса от Контроллера, переданного через Медиатор 
         {
             var accountEntity = _mapper.Map<Account>(request);
-            var newAccountId = await _accountRepository.AddAsync(accountEntity);
-
+            var newAccountId = await _accountRepository.AddAsync(accountEntity);      
+            // 1. Отправка события о новом пользователе должна быть выполнена после сохранения в базе данных? Требуется создать отдельный класс для взаимодействия с интерфейсом работы с ивентами или же можно обратиться к интерфейсу из этого класса?
+           
             return newAccountId; // newAccount.Account_ID
         }
 
